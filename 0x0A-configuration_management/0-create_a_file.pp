446075bea179fd4b7322971741347c1842d3f327
Esperalda <oluwapemimary@gmail.com>
2023-09-22 20:37:02 -0700

    Puppet Update 001

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a PP File
-file { '/tmp/holberton':
+file { '/tmp/school':
   ensure  => file, # Makes sure File Exists
-  path    => '/tmp/holberton', # PATH
+  path    => '/tmp/school', # PATH
   mode    => '0744',           # Permissions
   owner   => 'www-data',       # Owner
   group   => 'www-data',       # Group
